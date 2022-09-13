4639fca9ed5db325ffa74f09a2f7161a524275a5
Kimki <santichiste@hotmail.com>
2022-09-13 16:47:03 +0200

    fetch data images into home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,35 @@
-import type { NextPage } from 'next'
-import { ShopLayout } from '../components/layouts';
-import Typography from '@mui/material/Typography';
-
+import type { NextPage } from "next";
+import { ShopLayout } from "../components/layouts";
+import Typography from "@mui/material/Typography";
+import { Card, CardActionArea, CardMedia, Grid } from "@mui/material";
+import { initialData } from "../database/products";
 
 const Home: NextPage = () => {
   return (
-    <ShopLayout title={'Teslo-Shop -- HOME'} pageDescription={'Find your best products here at Teslo'}>
-      <Typography variant='h1' component='h1'>Home</Typography>
-      <Typography variant='h2' sx={{mb:1}}>All products</Typography>
-    </ShopLayout>
-  )
-}
+    <ShopLayout
+      title={"Teslo-Shop -- HOME"}
+      pageDescription={"Find your best products here at Teslo"}
+    >
+      <Typography variant="h1" component="h1">
+        Home
+      </Typography>
+      <Typography variant="h2" sx={{ mb: 1 }}>
+        All products
+      </Typography>
 
-export default Home
+      <Grid container spacing={4}>
+        {initialData.products.map((product) => (
+          <Grid item xs={6} sm={4} key={product.slug}>
+            <Card>
+              <CardActionArea>
+                <CardMedia component='img' image={`products/${product.images[0]}`} alt={product.title}/>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </ShopLayout>
+  );
+};
+
+export default Home;
